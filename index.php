753cac48a98d154d34aa3634a3eac2a24d09a7bf
dpereira3 <117852931+dpereira3@users.noreply.github.com>
2022-12-05 17:49:12 -0300

    Update index.php
    AGREGADOS VARIABLES DE ENTORNO

--- index.php
+++ index.php
@@ -4,6 +4,17 @@
 
 <a href="./view/username/create.php" class="btn btn-primary">Agregar nuevo usuario</a>
 
+<?php
+echo 'getenv ';
+echo getenv('CREATOR');
+echo '<br>';
+echo '$ env';
+echo $_ENV["CREATOR"];
+echo '<br>';
+echo '$ server';
+echo $_SERVER["CREATOR"];
+echo '<br>';
+?>
 
 <?php
     require_once('./view/head/footer.php');
